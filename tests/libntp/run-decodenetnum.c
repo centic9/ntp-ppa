5d263bcf2eff920a43ef33b8175c70b63aa0ee04
Dominik Stadler <dominik.stadler@gmx.at>
2023-01-20 07:43:10 +0100

    Import Upstream version 4.2.8p15+dfsg

--- tests/libntp/run-decodenetnum.c
+++ tests/libntp/run-decodenetnum.c
@@ -33,14 +33,19 @@ extern void test_IPv4AddressOnly(void);
 extern void test_IPv4AddressWithPort(void);
 extern void test_IPv6AddressOnly(void);
 extern void test_IPv6AddressWithPort(void);
+extern void test_IPv6AddressWithScope(void);
+extern void test_IPv6AddressWithPortAndScope(void);
 extern void test_IllegalAddress(void);
 extern void test_IllegalCharInPort(void);
+extern void test_NameBufOverflow(void);
 
 
 //=======Suite Setup=====
 static void suite_setup(void)
 {
+extern int change_iobufs(int);
 extern int change_logfile(const char*, int);
+change_iobufs(1);
 change_logfile("stderr", 0);
 }
 
@@ -65,8 +70,11 @@ int main(int argc, char *argv[])
   RUN_TEST(test_IPv4AddressWithPort, 9);
   RUN_TEST(test_IPv6AddressOnly, 10);
   RUN_TEST(test_IPv6AddressWithPort, 11);
-  RUN_TEST(test_IllegalAddress, 12);
-  RUN_TEST(test_IllegalCharInPort, 13);
+  RUN_TEST(test_IPv6AddressWithScope, 12);
+  RUN_TEST(test_IPv6AddressWithPortAndScope, 13);
+  RUN_TEST(test_IllegalAddress, 14);
+  RUN_TEST(test_IllegalCharInPort, 15);
+  RUN_TEST(test_NameBufOverflow, 16);
 
   return (UnityEnd());
 }
--- tests/libntp/sockaddrtest.c
+++ tests/libntp/sockaddrtest.c
@@ -26,18 +26,25 @@ IsEqual(const sockaddr_u expected, const sockaddr_u actual) {
 	}
 
 	if (actual.sa.sa_family == AF_INET) { // IPv4
-		if (expected.sa4.sin_port == actual.sa4.sin_port &&
-			memcmp(&expected.sa4.sin_addr, &actual.sa4.sin_addr,
-				   sizeof( in )) == 0) {
+		if (   expected.sa4.sin_port == actual.sa4.sin_port
+		    && memcmp(&expected.sa4.sin_addr, &actual.sa4.sin_addr,
+			      sizeof( in )) == 0) {
 			return TRUE;
 		} else {
-			printf("IPv4 comparision failed, expected: %s(%s) but was: %s(%s)",inet_ntoa(expected.sa4.sin_addr), socktoa(&expected), inet_ntoa(actual.sa4.sin_addr),socktoa(&actual));
+			char buf[4][32];
+			strlcpy(buf[0], inet_ntoa(expected.sa4.sin_addr), sizeof(buf[0]));
+			strlcpy(buf[1], socktoa(&expected)              , sizeof(buf[1]));
+			strlcpy(buf[2], inet_ntoa(actual.sa4.sin_addr)  , sizeof(buf[2]));
+			strlcpy(buf[3], socktoa(&actual)                , sizeof(buf[3]));
+			printf("IPv4 comparision failed, expected: %s(%s) but was: %s(%s)",
+			       buf[0], buf[1], buf[2], buf[3]);
 			return FALSE;
 		}
 	} else if (actual.sa.sa_family == AF_INET6) { //IPv6
-		if (expected.sa6.sin6_port == actual.sa6.sin6_port &&
-			memcmp(&expected.sa6.sin6_addr, &actual.sa6.sin6_addr,
-				   sizeof(in6)) == 0) {
+		if (   expected.sa6.sin6_port == actual.sa6.sin6_port
+		    && expected.sa6.sin6_scope_id == actual.sa6.sin6_scope_id
+		    && memcmp(&expected.sa6.sin6_addr, &actual.sa6.sin6_addr,
+			      sizeof(in6)) == 0) {
 			return TRUE;
 		} else {
 			printf("IPv6 comparision failed");
